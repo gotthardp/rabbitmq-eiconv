68623500136c2b2cad9e4b4fd4792b34a6f2845d
Petr Gotthard <petr.gotthard@centrum.cz>
2014-11-21 16:39:13 +0100

    Packege need to include priv as well.

--- package.mk
+++ package.mk
@@ -8,9 +8,12 @@ RETAIN_ORIGINAL_VERSION:=true
 ORIGINAL_APP_FILE:=$(CLONE_DIR)/src/eiconv.app.src
 DO_NOT_GENERATE_APP_FILE=true
 
-define package_rules
+CONSTRUCT_APP_PREREQS:=$(shell find $(CLONE_DIR)/priv -type f)
+define construct_app_commands
+	cp -r $(CLONE_DIR)/priv $(APP_DIR)
+endef
 
+define package_rules
 $(CLONE_DIR)/src/$(APP_NAME).app.src: $(CLONE_DIR)/.done
 	sed -i 's/git/"0.4.0"/' $(CLONE_DIR)/src/$(APP_NAME).app.src
-
 endef
